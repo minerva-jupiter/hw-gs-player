fa5892a2dfaa69efaa7afa9aa0d23b9b7a0c833e
Sunny <kajimalu10@gmail.com>
2025-03-14 20:39:34 +0900

    add vue-youtube, iframe

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,10 @@
 import { createApp } from 'vue'
+import { createManager } from '@vue-youtube/core';
 import './style.css'
 import App from './App.vue'
+import router from './router'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(createManager())
+app.use(router)
+app.mount('#app')
