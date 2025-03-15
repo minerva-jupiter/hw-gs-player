5d735fa7f3853b2898be853c8c83b43d12c21275
Sunny <kajimalu10@gmail.com>
2025-03-15 12:58:47 +0900

    update title-bar style

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,49 @@
 <script setup lang="ts">
-  let MenuButton: Boolean = true;
+import { ref, onMounted, onBeforeUnmount } from "vue";
+
+const MenuButton = ref(false);
+
+const closeMenu = (event: Event) => {
+  const target = event.target as HTMLElement;
+  if (!target.closest(".menu") && !target.closest(".hamburger_btn")) {
+    MenuButton.value = false;
+  }
+};
+
+onMounted(() => {
+  window.addEventListener("click", closeMenu);
+});
+
+onBeforeUnmount(() => {
+  window.removeEventListener("click", closeMenu);
+});
 </script>
 
 <template>
   <header>
     <nav>
-      <h2>GS Player</h2>
-      <div class="hamburger_btn" v-on:click="MenuButton=!MenuButton">
-        <span class="line line_01" v-bind:class="{'btn_line01':MenuButton}"></span>
-        <span class="line line_02" v-bind:class="{'btn_line02':MenuButton}"></span>
-        <span class="line line_03" v-bind:class="{'btn_line03':MenuButton}"></span>
+      <div class="title_bar">
+        <p>GS Player</p>
       </div>
-      <transition name="menu">
-        <div class="menu" v-show="MenuButton">
-          <ul>
-            <li><router-link to="/">Album List</router-link></li>
-            <li><router-link to="/about">about</router-link></li>
-            <li><router-link to="/terms">利用規約</router-link></li>
-            <li><router-link to="/enquiries">お問い合わせ</router-link></li>
-          </ul>
-        </div>
-      </transition>
+      <div class="hamburger_btn" @click.stop="MenuButton = !MenuButton">
+        <span class="line line_01" :class="{ 'btn_line01': MenuButton }"></span>
+        <span class="line line_02" :class="{ 'btn_line02': MenuButton }"></span>
+        <span class="line line_03" :class="{ 'btn_line03': MenuButton }"></span>
+      </div>
+      <div class="menu" :class="{ open: MenuButton }">
+        <ul>
+          <!-- 将来的には多言語対応する．中，韓ではそれぞれNoto sans C，Kを用いる -->
+          <li><router-link to="/" @click="MenuButton = false">Top</router-link></li>
+          <li><router-link to="/" @click="MenuButton = false">My List</router-link></li>
+          <hr>
+          <li><router-link to="/about" @click="MenuButton = false">About</router-link></li>
+          <li><router-link to="/terms" @click="MenuButton = false">利用規約</router-link></li>
+          <li><router-link to="/policy" @click="MenuButton = false">プライバシーポリシー</router-link></li>
+          <li><router-link to="/enquiries" @click="MenuButton = false">お問い合わせ</router-link></li>
+          <li><router-link to="/info" @click="MenuButton = false">運営者情報</router-link></li>
+        </ul>
+      </div>
     </nav>
   </header>
-  <router-view/>
+  <router-view />
 </template>
--- src/router.ts
+++ src/router.ts
@@ -1,21 +1,25 @@
-import {createRouter, createWebHistory} from 'vue-router';
-import TopView from "./components/AlbumList.vue";
+import { createRouter, createWebHistory } from 'vue-router';
+import Top from "./components/AlbumList.vue";
 import About from "./components/About.vue";
-import Terms from './components/Terms.vue';
 import Enquiries from './components/Enquiries.vue';
+import Info from './components/Info.vue';
 import Player from './components/Player.vue';
+import Policy from './components/Policy.vue';
+import Terms from './components/Terms.vue';
 
 const routes = [
-    {path: '/', name: 'top', component: TopView},
-    {path: '/about', name: 'about', component: About},
-    {path: '/terms', name: 'terms', component: Terms},
-    {path: '/enquiries', name: 'enquiries', component: Enquiries},
-    {path: '/player', name: 'player', component: Player},
+  { path: '/', name: 'top', component: Top },
+  { path: '/about', name: 'about', component: About },
+  { path: '/enquiries', name: 'enquiries', component: Enquiries },
+  { path: '/info', name: 'info', component: Info },
+  { path: '/player', name: 'player', component: Player },
+  { path: '/policy', name: 'policy', component: Policy },
+  { path: '/terms', name: 'terms', component: Terms },
 ]
 
 const router = createRouter({
-    history: createWebHistory(), // HTML5 History モード
-    routes,
+  history: createWebHistory(), // HTML5 History モード
+  routes,
 })
 
 export default router;
